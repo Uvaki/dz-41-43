78365af3e7f5b2054da3f158d12497210c66175a
uvaka <uvakin01@mail.ru>
2022-08-16 18:49:09 +0300

    дз 41

--- dz-41/Program.cs
+++ dz-41/Program.cs
@@ -1,34 +1,14 @@
-﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿
+// Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
 
-Console.Clear();
 
-Console.WriteLine($"Задача 41. Cколько чисел больше 0 ввёл пользователь ");
-Console.Write($"Введи число X(количество чисел): ");
-int x = Convert.ToInt32(Console.ReadLine());
-int[] massiveNumbers = new int[x];
 
-void InputNumbers(int x)
+int[] a = new int[10];
+Console.WriteLine("Введите 10 чисел.");
+for (int i = 0; i < a.Length; i++)
 {
-    for (int i = 0; i < x; i++)
-    {
-        Console.Write($"Введи {i + 1} число: ");
-        massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
-    }
+    Console.Write($"Введите a[{i}]: ");
+    a[i] = int.Parse(Console.ReadLine()); 
 }
-
-
-int Comparison(int[] massiveNumbers)
-{
-    int count = 0;
-    for (int i = 0; i < massiveNumbers.Length; i++)
-    {
-        if (massiveNumbers[i] > 0) count += 1;
-    }
-    return count;
-}
-
-InputNumbers(x);
-
-Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
